b142fb1ed7c8c12a15f250d7bfe95ac9ca6f3baa
emreocak1 <emreocak2332@gmail.com>
2025-09-10 22:24:13 +0300

    implementing passport.js and completing user resolver

--- server/index.js
+++ server/index.js
@@ -5,17 +5,49 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv'
-
+import passport from 'passport'
+import session from 'express-session';
+import connectMongo from 'connect-mongodb-session'; //? ConnectMongoDBSession
+import { buildContext } from 'graphql-passport';
 
 import mergedResolvers from './resolvers/index.js'
 import mergedTypeDefs from "./typeDefs/index.js";
 
 import {connectDB} from './database/connectDB.js'
+import {configurePassport} from './passport/passport.config.js'
 
 dotenv.config()
+configurePassport()
 
 const app = express()
 const httpServer = http.createServer(app)
+
+const MongoDBStore = connectMongo(session)
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions"
+})
+
+store.on('error',(error)=>{
+  console.log(error)
+})
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //* Specifies whether to save the session to the store on every request
+    saveUninitialized: false, //* Specifies whether to save the uninitialized sessions
+    cookie: {
+      maxAge: 1000*60*60*24*7, // 1 WEEK
+      httpOnly: true //! Preventing from XSS attacks
+    },
+    store: store
+  })
+)
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -27,14 +59,14 @@ await server.start()
 
 app.use(
   '/',
-  cors(),
+  cors({ origin:'http://localhost:3000',credentials:true }),
   express.json(),
   expressMiddleware(server,{
-    context: async({req}) => ({req})
+    context: async({req,res}) => buildContext({req,res})
   })
 )
 
 await new Promise((resolve)=> httpServer.listen({port:4000},resolve))
 await connectDB()
 
-console.log('Server start at port 4000');
+console.log('Server start at port 3000');
--- server/resolvers/user.resolver.js
+++ server/resolvers/user.resolver.js
@@ -1,16 +1,108 @@
 import {users} from '../dummyData/data.js'
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+
 
 const userResolver = {
-  Query: {
-    users: () => {
-      return users
+  Mutation: {
+    signUp: async(_,{input},context) => {
+
+      try {
+        const {username, name, password, gender} = input
+
+        if(!username || !name || !password || !gender) throw new Error('All field needs to be filled')
+
+        const existingUser = await User.findOne({username})
+        if(existingUser) throw new Error('User already exists')
+
+        //* Disguise the password 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        //* API request for profile pics
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        //* Saving user to the database
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === 'male' ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save()
+        await context.login(newUser)
+        return newUser
+
+      } catch (error) {
+        console.error('Error in create user',error)
+        throw new Error(error.message || 'Internal server error')
+      }
     },
-    user: (_,{userId}) => {
-      return users.find((user)=>user._id === userId)
-    }
+
+
+    login: async(_,{input},context) => {
+
+      try {
+        const {username, password} = input
+        const {user} = await context.authenticate('graphql-local',{username,password})
+
+        await context.login(user)
+        return user
+      } catch (error) {
+        console.error('Error when login',error)
+        throw new Error(error.message || 'Internal server error')
+      }
+    },
+
+
+    logout: async(_,__,context) => {
+
+      try {
+
+        await context.logout()
+        req.session.destroy((error)=>{
+          if(error) throw new Error
+        })
+        res.clearCookie('connect.sid')
+        return {message:'Logged out successfully'}
+
+      } catch (error) {
+        console.error('Error when logout',error)
+        throw new Error(error.message || 'Internal server error')
+      }
+    },
+
   },
 
-  Mutation: {}
+
+  Query: {
+    authUser: async(_,__,context) => {
+
+      try {
+        const user = await context.getUser()
+        return user
+      } catch (error) {
+        console.error('Error in authentication',error)
+        throw new Error('Internal server error')
+      }
+
+    },
+
+    user: async(_,{userId}) => {
+      
+      try {
+        const user = await User.findById(userId)
+        return user
+      } catch (error) {
+        console.error('Error in user query',error)
+        throw new Error(error.message || 'Internal server error')
+      }
+
+    }
+  },
 }
 
 export default userResolver
